8e31bddc6cc4e579cd5333541712bd8456b77e63
w <36293421+ppond454@users.noreply.github.com>
2022-05-10 01:44:12 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import { createServer } from "http"
 import { Server } from "socket.io"
+import express from "express"
+import eiows from "eiows"
+import "dotenv/config"
 
-const httpServer = createServer()
+const app = express()
+
+const PORT = process.env.PORT || 4000
+const URL = process.env.URL_DEPLOYMENT || "http://localhost:3000"
+
+const httpServer = createServer(app)
 const io = new Server(httpServer, {
-  cors:{
-    origin: "*",
-  }
+  cors: {
+    origin: URL,
+  },
+  wsEngine: eiows.Server,
 })
 
 let users = []
@@ -24,39 +33,32 @@ const getUser = (userId) => {
 }
 
 io.on("connection", (socket) => {
-  //when ceonnect
   console.log("a user connected.")
-  // console.log(socket.id)
 
-  //take userId and socketId from user
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id)
     io.emit("getUsers", users)
   })
 
-  //send and get message
-  socket.on("sendMessage", ({ from, to, text ,createdAt , media }) => {
-    console.log("to: ",to);
+  socket.on("sendMessage", ({ from, to, text, createdAt, media }) => {
+    console.log("to: ", to)
     const user = getUser(to)
 
     if (!user) return null
-    if(user)
-    io.to(user.socketId).emit("getMessage", {
-      to,
-      from,
-      text,
-      createdAt,
-      media,
-    })
+    if (user)
+      io.to(user.socketId).emit("getMessage", {
+        to,
+        from,
+        text,
+        createdAt,
+        media,
+      })
   })
 
-  //when disconnect
   socket.on("disconnect", () => {
     console.log("a user disconnected!")
     removeUser(socket.id)
     io.emit("getUsers", users)
   })
-
-
 })
-httpServer.listen(4000)
+httpServer.listen(PORT, () => console.log("server is running on port 4000"))
